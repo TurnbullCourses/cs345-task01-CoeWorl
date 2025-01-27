48a1ea72b326f3f4c4f4576b9c88c25541411acc
Ben Summer <jackwanamaker49@gmail.com>
2025-01-26 22:07:32 -0500

    Uploaded some of my progress to isEmailValid

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -40,11 +40,25 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+        int atChar = email.indexOf('@');
+        if (email.substring(0,0).equals(".") || email.substring(0,0).equals("-") || email.substring(0,0).equals("_")){
             return false;
         }
-        else {
-            return true;
+        for (int i=1; i<atChar; i++){
+            if (!Character.isLetterOrDigit(email.charAt(i-1)) && !Character.isLetterOrDigit(email.charAt(i))){
+                return false;
+            }
         }
+        for (int i = atChar + 1; i<email.length(); i++){
+            if (!Character.isLetterOrDigit(email.charAt(i-1)) && email.charAt(i) != '.' && email.charAt(i) != '-' && email.charAt(i) != '_'){
+                return false;
+            }
+        }
+        for (int i = 1; i <email.length(); i++){
+            if (!(Character.isLetterOrDigit(email.charAt(i)) || email.charAt(i) == '.' || email.charAt(i) == '-' || email.charAt(i) == '_') || (email.charAt(i) == '@' && i == atChar)){
+                return false;
+            }
+        }
+
     }
 }
